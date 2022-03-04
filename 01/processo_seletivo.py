8e1ae2f4200da936a36faaff5dd30eae39e0794f
João Leite <joaoaugustobr@hotmail.com>
2022-03-04 17:42:51 -0300

    adding mergesort

--- 01/processo_seletivo.py
+++ 01/processo_seletivo.py
@@ -1,3 +1,40 @@
+# Caio Ueno e João Augusto Leite
+
+def mergesort(arr):
+    if len(arr) > 1:
+        i_mid = len(arr)//2
+
+        # divisao do array em duas metades 
+        left_arr = arr[:i_mid]
+        right_arr = arr[i_mid:]
+        mergesort(left_arr)
+        mergesort(right_arr)
+
+        # merge dos dois lados no array principal
+        i, j, k = 0, 0, 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] > right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        
+        # se ainda faltam elementos em algum dos lados, transfere todos
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+    return arr
+
+
 if __name__ == "__main__":
     N = int(input())
     for i in range(N):
@@ -5,3 +42,5 @@ if __name__ == "__main__":
         K = int(user_input_list[0])
         C = int(user_input_list[1])
         scores = [float(score) for score in user_input_list[2:C]]
+        scores_sorted = mergesort(scores)
+        print(scores_sorted[K-1])
