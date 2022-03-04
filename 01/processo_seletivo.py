8d2dbc9d793cf41cd7e2458b3463611f707b0fea
João Leite <joaoaugustobr@hotmail.com>
2022-03-04 18:00:01 -0300

    formatting float to always print 2 decimals

--- 01/processo_seletivo.py
+++ 01/processo_seletivo.py
@@ -43,4 +43,4 @@ if __name__ == "__main__":
         C = int(user_input_list[1])
         scores = [float(score) for score in user_input_list[2:]]
         scores_sorted = mergesort(scores)
-        print(scores_sorted[K-1])
+        print(format(scores_sorted[K-1], ".2f"))
